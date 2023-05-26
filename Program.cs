dcd5cdcce7224d3bdf5b106c82cf728373b9f5e0
Sergey11101975 <bogoslovskiisv@yandex.ru>
2023-05-26 08:53:40 +0300

    домашнее задание

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information */
+/*
+int numberA = 7;
+int numberB = 5;
+Console.WriteLine(numberA > numberB);
+
+
+напишите программу принимает 3 числа и выдаёт максимум
+
+*/ /*
+int a = 2;
+int b = 3;
+int c= 7;
+int max = 2;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.Write("max= ");
+Console.WriteLine(max);
+
+
+Console.WriteLine(" введите 2 числа");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+if (a > b){
+ Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
